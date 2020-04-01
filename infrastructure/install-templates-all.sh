80b28954950ef3b9432a6381ba5c4f475a3a826c
Robin Klemens <robin.klemens@deutschebahn.com>
2020-04-01 16:06:10 +0200

    added chaincode

--- infrastructure/install-templates-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./install-templates.sh bccekvk oc
-./install-templates.sh bcdcekvk oc
-./install-templates.sh bcqacekvk oc
-./install-templates.sh bcuicekvk oc
-./install-templates.sh bccicd oc
