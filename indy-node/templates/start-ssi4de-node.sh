e8554daf45a0b1fd16f70dfc388e57e230a31eb8
Robin Klemens <robin.klemens@deutschebahn.com>
2020-08-03 15:36:16 +0200

    test

--- indy-node/templates/start-ssi4de-node.sh
+++ indy-node/templates/start-ssi4de-node.sh
@@ -8,7 +8,7 @@ SEED_PHRASE=$3
 
 cd /tmp
 git clone https://github.com/CHempel-esatus/SSI4DE_Genesis.git
-cd SSI4DE_Genesis
+cd /tmp/SSI4DE_Genesis
 
 # Get CheckSum from read.me file
 SHA256_DOMAIN_TRANSACTION_GENESIS=$(sed '5q;d' README.md)
@@ -18,8 +18,14 @@ SHA256_POOL_TRANSACTION_GENESIS=$(sed '7q;d' README.md)
 SHA256_DOMAIN_TRANSACTION_GENESIS_CHECKSUM=$(sha256sum domain_transactions_genesis | awk '{print $1}')
 SHA256_POOL_TRANSACTION_GENESIS_CHECKSUM=$(sha256sum pool_transactions_genesis | awk '{print $1}')
 
-if [ "${SHA256_DOMAIN_TRANSACTION_GENESIS}" = "${SHA256_DOMAIN_TRANSACTION_GENESIS_CHECKSUM}" ] && [ "${SHA256_POOL_TRANSACTION_GENESIS}" = "${SHA256_POOL_TRANSACTION_GENESIS_CHECKSUM}" ]; then
-    init_indy_node ${NODE_ALIAS} ${SERVICE_IP} 9701 ${SERVICE_IP} 9702 ${SEED_PHRASE} && start_indy_node ${NODE_ALIAS} ${SERVICE_IP} 9701 ${SERVICE_IP} 9702
-else
-    echo "CHECKSUM DOESN'T MATCH!!!! ERROR!!!!"
-fi
+# if [ "${SHA256_DOMAIN_TRANSACTION_GENESIS}" = "${SHA256_DOMAIN_TRANSACTION_GENESIS_CHECKSUM}" ] && [ "${SHA256_POOL_TRANSACTION_GENESIS}" = "${SHA256_POOL_TRANSACTION_GENESIS_CHECKSUM}" ]; then
+#     mv /tmp/domain_transactions_genesis /var/lib/indy/ssi4de_test
+#     mv /tmp/pool_transactions_genesis /var/lib/indy/ssi4de_test
+#     init_indy_node ${NODE_ALIAS} ${SERVICE_IP} 9701 ${SERVICE_IP} 9702 ${SEED_PHRASE} && start_indy_node ${NODE_ALIAS} ${SERVICE_IP} 9701 ${SERVICE_IP} 9702
+# else
+#     echo "CHECKSUM DOESN'T MATCH!!!! ERROR!!!!"
+# fi
+
+mv /tmp/domain_transactions_genesis /var/lib/indy/ssi4de_test
+mv /tmp/pool_transactions_genesis /var/lib/indy/ssi4de_test
+init_indy_node ${NODE_ALIAS} ${SERVICE_IP} 9701 ${SERVICE_IP} 9702 ${SEED_PHRASE} && start_indy_node ${NODE_ALIAS} ${SERVICE_IP} 9701 ${SERVICE_IP} 9702
