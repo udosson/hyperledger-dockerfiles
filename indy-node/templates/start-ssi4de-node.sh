e5c925291a758be44a93942a6c46b9ef149d1934
Robin Klemens <robin.klemens@deutschebahn.com>
2020-08-07 10:37:45 +0200

    .

--- indy-node/templates/start-ssi4de-node.sh
+++ indy-node/templates/start-ssi4de-node.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-# Sets new /etc/passwd file
+# Sets new /etc/passwd file with indy user
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 envsubst < /tmp/passwd.template > /tmp/passwd
 export LD_PRELOAD=/usr/lib/libnss_wrapper.so
 export NSS_WRAPPER_PASSWD=/tmp/passwd
 export NSS_WRAPPER_GROUP=/etc/group
-
 echo "Indy node prepared to run as user indy"
 
 
@@ -17,29 +16,33 @@ NODE_ALIAS=$1
 SERVICE_IP=$2
 SEED_PHRASE=$3
 
+# Clone SSI4DE genesis files
 cd /tmp
 git clone https://github.com/CHempel-esatus/SSI4DE_Genesis.git
 cd /tmp/SSI4DE_Genesis
 
 # Get CheckSum from read.me file
 SHA256_DOMAIN_TRANSACTION_GENESIS=$(sed '5q;d' README.md)
-SHA256_POOL_TRANSACTION_GENESIS=$(sed '7q;d' README.md)
+# SHA256_POOL_TRANSACTION_GENESIS=$(sed '7q;d' README.md)
 
 # Calculte Checksum
 SHA256_DOMAIN_TRANSACTION_GENESIS_CHECKSUM=$(sha256sum domain_transactions_genesis | awk '{print $1}')
-SHA256_POOL_TRANSACTION_GENESIS_CHECKSUM=$(sha256sum pool_transactions_genesis | awk '{print $1}')
+# SHA256_POOL_TRANSACTION_GENESIS_CHECKSUM=$(sha256sum pool_transactions_genesis | awk '{print $1}')
 
-# if [ "${SHA256_DOMAIN_TRANSACTION_GENESIS}" = "${SHA256_DOMAIN_TRANSACTION_GENESIS_CHECKSUM}" ] && [ "${SHA256_POOL_TRANSACTION_GENESIS}" = "${SHA256_POOL_TRANSACTION_GENESIS_CHECKSUM}" ]; then
-#     mv /tmp/domain_transactions_genesis /var/lib/indy/ssi4de_test
-#     mv /tmp/pool_transactions_genesis /var/lib/indy/ssi4de_test
-#     init_indy_node ${NODE_ALIAS} ${SERVICE_IP} 9701 ${SERVICE_IP} 9702 ${SEED_PHRASE} && start_indy_node ${NODE_ALIAS} ${SERVICE_IP} 9701 ${SERVICE_IP} 9702
-# else
-#     echo "CHECKSUM DOESN'T MATCH!!!! ERROR!!!!"
-# fi
+if [ "${SHA256_DOMAIN_TRANSACTION_GENESIS}" = "${SHA256_DOMAIN_TRANSACTION_GENESIS_CHECKSUM}" ]; then
+    mv /tmp/domain_transactions_genesis /var/lib/indy/ssi4de_test
+    # mv /tmp/pool_transactions_genesis /var/lib/indy/ssi4de_test
+    init_indy_node ${NODE_ALIAS} ${SERVICE_IP} 9701 ${SERVICE_IP} 9702 ${SEED_PHRASE}
+    start_indy_node ${NODE_ALIAS} ${SERVICE_IP} 9701 ${SERVICE_IP} 9702
+else
+    echo "CHECKSUM DOESN'T MATCH!!!! ERROR!!!!"
+fi
 
-# mv /tmp/SSI4DE_Genesis/domain_transactions_genesis /var/lib/indy/ssi4de_test
-mv /tmp/SSI4DE_Genesis/pool_transactions_genesis /var/lib/indy/ssi4de_test
-init_indy_node ${NODE_ALIAS} ${SERVICE_IP} 9701 ${SERVICE_IP} 9702 ${SEED_PHRASE}
-start_indy_node ${NODE_ALIAS} ${SERVICE_IP} 9701 ${SERVICE_IP} 9702
+# # Move genesis files
+# # mv /tmp/SSI4DE_Genesis/domain_transactions_genesis /var/lib/indy/ssi4de_test
+# mv /tmp/SSI4DE_Genesis/pool_transactions_genesis /var/lib/indy/ssi4de_test
 
-
+# # Init indy node with crypto material
+# init_indy_node ${NODE_ALIAS} ${SERVICE_IP} 9701 ${SERVICE_IP} 9702 ${SEED_PHRASE}
+# # Start indy node
+# start_indy_node ${NODE_ALIAS} ${SERVICE_IP} 9701 ${SERVICE_IP} 9702
