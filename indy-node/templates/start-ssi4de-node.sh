32ebb7e942886ae56a4253b77cdaffbeeb70de11
Robin Klemens <robin.klemens@deutschebahn.com>
2020-08-07 11:02:46 +0200

    added echo

--- indy-node/templates/start-ssi4de-node.sh
+++ indy-node/templates/start-ssi4de-node.sh
@@ -28,8 +28,10 @@ SHA256_DOMAIN_TRANSACTION_GENESIS=$(sed '5q;d' README.md)
 # Calculte Checksum
 SHA256_DOMAIN_TRANSACTION_GENESIS_CHECKSUM=$(sha256sum domain_transactions_genesis | awk '{print $1}')
 # SHA256_POOL_TRANSACTION_GENESIS_CHECKSUM=$(sha256sum pool_transactions_genesis | awk '{print $1}')
+echo "SHA256_DOMAIN_TRANSACTION_GENESIS: ${SHA256_DOMAIN_TRANSACTION_GENESIS}"
+echo "SHA256_DOMAIN_TRANSACTION_GENESIS_CHECKSUM: ${SHA256_DOMAIN_TRANSACTION_GENESIS_CHECKSUM}"
 
-if [ "${SHA256_DOMAIN_TRANSACTION_GENESIS}" -eq "${SHA256_DOMAIN_TRANSACTION_GENESIS_CHECKSUM}" ]; then
+if [ "${SHA256_DOMAIN_TRANSACTION_GENESIS}" = "${SHA256_DOMAIN_TRANSACTION_GENESIS_CHECKSUM}" ]; then
     mv /tmp/domain_transactions_genesis /var/lib/indy/ssi4de_test
     # mv /tmp/pool_transactions_genesis /var/lib/indy/ssi4de_test
     init_indy_node ${NODE_ALIAS} ${SERVICE_IP} 9701 ${SERVICE_IP} 9702 ${SEED_PHRASE}
